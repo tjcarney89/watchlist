519e2653feb5ee985cab3ef068438a2f2d1782bb
tjcarney89 <tjcarney@stny.rr.com>
2018-11-14 13:18:19 -0500

    Initial Commit

---
+++ Watchlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Watchlist
+//
+//  Created by TJ Carney on 11/14/18.
+//  Copyright © 2018 TJ Carney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
